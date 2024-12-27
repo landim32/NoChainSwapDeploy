86d6b48c07cc27b563c1c210e758662b4bbfaf1c
rodrigo.carneiro <rodrigo.carneiro@benner.com.br>
2024-12-26 23:32:59 -0300

    first commit

---
+++ Frontend/big-whale/env.js
@@ -0,0 +1,12 @@
+window.env = {
+  "PROJECT_NAME": "Big Security Bank",
+  "NODE_PATH": "./src",
+  "PUBLIC_URL": "/big-whale",
+  "SITE_BASENAME": "/big-whale",
+  "API_BASE_URL": "https://localhost:44374",
+  "USDT_CONTRACT": "0x7ef95a0FEE0Dd31b22626fA2e10Ee6A223F8a684",
+  "PORT": "443",
+  "USE_STACKS_CHAIN": false,
+  "USE_BITCOIN_SWAP": false,
+  "USER_BRL_SWAP": true
+};
---
+++ Frontend/nochainswap/env.js
@@ -0,0 +1,12 @@
+window.env = {
+  "PROJECT_NAME": "NoChainSwap.org",
+  "NODE_PATH": "./src",
+  "PUBLIC_URL": "/",
+  "SITE_BASENAME": "/",
+  "API_BASE_URL": "https://localhost:44374",
+  "USDT_CONTRACT": "0x7ef95a0FEE0Dd31b22626fA2e10Ee6A223F8a684",
+  "PORT": "443",
+  "USE_STACKS_CHAIN": true,
+  "USE_BITCOIN_SWAP": true,
+  "USER_BRL_SWAP": false
+};
